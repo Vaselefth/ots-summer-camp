54653ac6e1115c280047d8293b5ec18512660223
Eleftheriadis Vasilis <vaselefth1@gmail.com>
2021-08-11 13:45:26 +0300

    Delete ots-sql-insert-values-script.sql

--- ots-sql-insert-values-script.sql
+++
@@ -1,5 +0,0 @@
-insert into ots_summer_camp.vat (id, vat_value, valid_date, vat_description)
-values (1,0.13,'2011-12-18 13:17:17','kappa');
-
-insert into ots_summer_camp.product_service (id, product_description, price_per_item, discount, is_product, vat_id)
-values (1,'kappaKeppo', 10.2, 2.2, 1, 1);
