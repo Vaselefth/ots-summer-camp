c495d6b42807a20b9d03cba14cd0f0692c9b8b5b
Eleftheriadis Vasilis <vaselefth1@gmail.com>
2021-08-12 09:57:26 +0300

    Delete demo directory

--- demo/src/main/java/com/testOTS/demo/demo/dao/TransactorRepository.java
+++
@@ -1,8 +0,0 @@
-package com.testOTS.demo.demo.dao;
-
-import com.testOTS.demo.demo.entity.Transactor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TransactorRepository extends JpaRepository<Transactor, Long> {
-
-}
