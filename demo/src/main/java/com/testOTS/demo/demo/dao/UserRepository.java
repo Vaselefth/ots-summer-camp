26c47bc539b26722058ebd3861bfacc53eeb31bf
Γιώργος Αντωνιάδης <65530603+George-Anto@users.noreply.github.com>
2021-08-25 12:44:37 +0300

    Update UserRepository.java

--- demo/src/main/java/com/testOTS/demo/demo/dao/UserRepository.java
+++ demo/src/main/java/com/testOTS/demo/demo/dao/UserRepository.java
@@ -4,5 +4,7 @@ import com.testOTS.demo.demo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+  
+  User findByUsernameAndPassword(String username, String password);
 
 }
