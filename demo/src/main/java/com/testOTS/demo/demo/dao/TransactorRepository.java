13c94147fdd0fc43895dc54605b82a32a448d171
Eleftheriadis Vasilis <vaselefth1@gmail.com>
2021-08-25 09:47:26 +0300

    Update TransactorRepository.java

--- demo/src/main/java/com/testOTS/demo/demo/dao/TransactorRepository.java
+++ demo/src/main/java/com/testOTS/demo/demo/dao/TransactorRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TransactorRepository extends JpaRepository<Transactor, Long> {
 
+    Transactor findByTin(long tin);
+
 }
