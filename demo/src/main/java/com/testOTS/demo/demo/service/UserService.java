f597e272cdded124cac86ed7c65d7de4b8ba68e8
Γιώργος Αντωνιάδης <65530603+George-Anto@users.noreply.github.com>
2021-08-25 12:45:37 +0300

    Update UserService.java

--- demo/src/main/java/com/testOTS/demo/demo/service/UserService.java
+++ demo/src/main/java/com/testOTS/demo/demo/service/UserService.java
@@ -13,4 +13,6 @@ public interface UserService {
     public void save(User user);
 
     public void deleteById(long theId);
+    
+    User findByUsernameAndPassword(String username, String password);
 }
